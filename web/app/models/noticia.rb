83bd44a7990f5045c632ddf9dabfa45fba2ea704
Diego <dr.nexus84@gmail.com>
2010-07-28 13:05:23 +0200

    cambios del último día.

--- web/app/models/noticia.rb
+++ web/app/models/noticia.rb
@@ -1,2 +1,11 @@
 class Noticia < ActiveRecord::Base
+
+	def self.encuentra_ultimas_noticias
+		find(:all, :order =>"titular")
+	end
+	validates_presence_of 	:titular, :cuerpo, :autor, :image_url
+
+	validates_format_of 	:image_url,
+				:with	=>%r{\.(gif|jpg|png)$}i,
+				:message	=> 'La imagen debe ser de tipo jpg, png o gif.'
 end
